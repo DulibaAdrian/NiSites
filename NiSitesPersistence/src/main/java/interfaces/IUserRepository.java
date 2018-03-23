9a9e9b73c044bcdcca46f47db6007dac3fb1d6c2
duliba.adrian <adrian.duliba@issco.ro>
2018-03-23 11:13:52 +0200

    REST Repository Update

--- NiSitesPersistence/src/main/java/interfaces/IUserRepository.java
+++ NiSitesPersistence/src/main/java/interfaces/IUserRepository.java
@@ -1,4 +1,4 @@
-package entities.repositoryInterfaces;
+package interfaces;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- NiSitesPersistence/src/main/java/rest/controllers/UserController.java
+++ NiSitesPersistence/src/main/java/rest/controllers/UserController.java
@@ -1,15 +1,17 @@
-package rest.services;
+package rest.controllers;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import entities.domain.User;
-import entities.repositoryInterfaces.IUserRepository;
+import interfaces.IUserRepository;
 
 @RestController
 @RequestMapping("/api/user")
@@ -21,5 +23,15 @@ public class UserController {
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public List<User> getUserList() {
 		return userRepository.findAll();
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void addUser(@RequestBody User user) {
+		this.userRepository.save(user);
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.PUT)
+	public void editUser(@PathVariable Integer userId, @RequestBody User user) {
+		 //this.userRepository.save(user);
 	}
 }
