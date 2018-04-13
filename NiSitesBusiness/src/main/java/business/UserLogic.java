478903c26f919816c6f24ac98088f8fb92dd8bb6
duliba.adrian <adrian.duliba@issco.ro>
2018-04-13 18:33:17 +0300

    Add some business

--- NiSitesBusiness/src/main/java/business/UserLogic.java
+++ NiSitesBusiness/src/main/java/business/UserLogic.java
@@ -1,21 +1,36 @@
 package business;
 
+import java.util.UUID;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import business.DTO.UserDTO;
 
 @RestController
 @RequestMapping("/api/business/user")
 public class UserLogic {
 
+	String url="http://localhost:8080/api/repository/user/";
+	RestTemplate restTemplate = new RestTemplate();
+
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
 	public UserDTO[] getUserList() {
-		RestTemplate restTemplate = new RestTemplate();
-		UserDTO[] listUsers = restTemplate.getForObject("http://localhost:8080/api/repository/user/", UserDTO[].class);		
+		UserDTO[] listUsers = restTemplate.getForObject(this.url, UserDTO[].class);		
 		return listUsers;
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public void addUser(@RequestBody UserDTO userDTO) {
+		restTemplate.postForEntity(this.url, userDTO, UserDTO.class);
+	}
+	
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+	public UserDTO getUserById(@PathVariable UUID userId) {
+		UserDTO userDTO = restTemplate.getForObject(this.url+userId.toString(), UserDTO.class);		
+		return userDTO;
 	}
 }
